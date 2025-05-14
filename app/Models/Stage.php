31a96350b7b89e3a64ae3b30ff8ff797810ee124
deefyy <notdeefyy@gmail.com>
2025-05-15 00:28:29 +0200

    Added new excel export

--- app/Models/Stage.php
+++ app/Models/Stage.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\StagesCompetition;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,5 +17,10 @@ class Stage extends Model
 
     public function competition() {
         return $this->belongsTo(Competition::class);
+    }
+
+    public function results()
+    {
+        return $this->hasMany(StageCompetition::class, 'stage_id');
     }
 }
