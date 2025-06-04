f878872b57144eb457481465f5082adc9c67b044
Deefyy <15126011+deefyy@users.noreply.github.com>
2025-06-04 09:44:54 +0000

    Zmieniono nazwę tabeli na stage

--- database/migrations/2025_06_04_113905_add_float_to_results.php
+++ database/migrations/2025_06_04_113905_add_float_to_results.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('stages', function (Blueprint $table) {
+        Schema::table('stage', function (Blueprint $table) {
             $table->float('result')->nullable()->change();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('stages', function (Blueprint $table) {
+        Schema::table('stage', function (Blueprint $table) {
             $table->string('result')->nullable()->change();
         });
     }
